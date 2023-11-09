105203094a505e840b2b4f7164abaf9745ced231
Cezary Gawinek <cezarygawinek@gmail.com>
2023-11-09 12:04:23 +0100

    zzzz

---
+++ moduly.py
@@ -0,0 +1,16 @@
+# #import math
+# from math import sqrt
+
+# print(math.sqrt(10))
+#==================================
+import random
+
+print(random.randint(1,100))
+#losowa wartosc z listy
+fruits = ["apple","banana","orange"]
+print(random.choice(fruits))
+
+#mieszanie listy:
+numbers=[1,2,3,4,5]
+random.shuffle(numbers)
+print(numbers)
